fcccc3b114be3ad0fc1884e98f858b3670a2efbf
Bowen Zhong <bz4e14@soton.ac.uk>
2017-09-22 18:34:00 +0800

    parser

--- parser.js
+++ parser.js
@@ -159,110 +159,236 @@ const tokens1 = [
 function parser(tokens) {
   let current = 0;
 
-  function walk() {
-    const token = tokens[current];
+  let token = tokens[current];
+  let node;
 
-    if (token.type === 'Keyword') {
-      if (token.value === 'function') {
-        if (tokens[current + 1].type === 'Identifier') {
+  if (token.type === 'Keyword') {
+    if (token.value === 'function') {
+      if (tokens[current + 1].type === 'Identifier') {
 
-          const node = {
-            "type": "FunctionDeclaration",
-            "id": {
-              "type": "Identifier",
-              "name": tokens[++current].value
-            },
-            "params": []
-          };
+        node = {
+          "type": "FunctionDeclaration",
+          "id": {
+            "type": "Identifier",
+            "name": tokens[++current].value
+          },
+          "params": [],
+          "body": {}
+        };
 
+        ++current;
+        if (tokens[current].type === "Punctuator" && tokens[current].value === "(") {
           ++current;
-          if (tokens[current].type === "Punctuator" && tokens[current].value === "(") {
-            ++current;
-            while (
-              tokens[current] && tokens[current].type === "Identifier"
-            ) {
-              if (tokens[current+1].type === "Punctuator") {
-                switch (tokens[current+1].value) {
-                  case ')':
+          while (
+            tokens[current] && tokens[current].type === "Identifier"
+          ) {
+            if (tokens[current + 1].type === "Punctuator") {
+              switch (tokens[current + 1].value) {
+                case ')':
+                  node.params.push({
+                    "type": "Identifier",
+                    "name": tokens[current]
+                  });
+                  current += 1;
+                  break;
+                case ',':
+                  node.params.push({ "type": "Identifier", "name": tokens[current].value });
+                  current += 2;
+                  continue;
+                case '=':
+                  if (tokens[current + 2].type === 'String') {
                     node.params.push({
-                      "type": "Identifier",
-                      "name": tokens[current]
-                    });
-                    current+=2;
-                    break;
-                  case ',':
-                    node.params.push({ "type": "Identifier", "name": tokens[current].value });
-                    current+=2;
-                    continue;
-                  case '=':
-                    if (tokens[current + 2].type === 'String') {
-                      node.params.push({
-                        "type": "AssignmentPattern",
-                        "left": {
-                          "type": "Identifier",
-                          "name": tokens[current].value
-                        },
-                        "right": {
-                          "type": "Literal",
-                          "value": tokens[current + 2].value,
-                          "raw": tokens[current + 2].value
-                        }
-                      });
-
-                      if(tokens[current+3].type==="Punctuator") {
-                        switch (tokens[current+3].value) {
-                          case ')':
-                            current+=3;
-                            break;
-                          case ',':
-                            current+=4;
-                            continue;
-                          default:
-                            throw new TypeError('Uncaught SyntaxError: Unexpected ' + tokens[current].type);
-                        }
+                      "type": "AssignmentPattern",
+                      "left": {
+                        "type": "Identifier",
+                        "name": tokens[current].value
+                      },
+                      "right": {
+                        "type": "Literal",
+                        "value": tokens[current + 2].value,
+                        "raw": tokens[current + 2].value
                       }
-                      continue;
-                    } else {
-                      throw new TypeError('Uncaught SyntaxError: Unexpected ' + tokens[current].type);
+                    });
+
+                    if (tokens[current + 3].type === "Punctuator") {
+                      switch (tokens[current + 3].value) {
+                        case ')':
+                          current += 3;
+                          break;
+                        case ',':
+                          current += 4;
+                          continue;
+                        default:
+                          throw new TypeError('Uncaught SyntaxError: Unexpected ' + tokens[current].type);
+                      }
                     }
-                  default:
-                    throw new TypeError('Uncaught SyntaxError: Unexpected ' + tokens[current].value);
-                    break;
-                }
-
-              } else {
-                throw new TypeError('Uncaught SyntaxError: Unexpected ' + tokens[current].type);
+                    continue;
+                  } else {
+                    throw new TypeError('Uncaught SyntaxError: Unexpected ' + tokens[current].type);
+                  }
+                default:
+                  throw new TypeError('Uncaught SyntaxError: Unexpected ' + tokens[current].value);
+                  break;
               }
-            }
 
-            if (
-              tokens[current] &&
-              !(tokens[current].type === "Punctuator" && tokens[current].value === ")")
-            ) {
-              throw new TypeError('Uncaught SyntaxError: Unexpected ' + tokens[current].value);
+            } else {
+              throw new TypeError('Uncaught SyntaxError: Unexpected ' + tokens[current].type);
             }
+          }
 
-            return node;
-            debugger;
-          } else {
-            throw new TypeError('Uncaught SyntaxError: Unexpected ' + tokens[current].type);
+          if (
+            tokens[current] &&
+            !(tokens[current].type === "Punctuator" && tokens[current].value === ")")
+          ) {
+            throw new TypeError('Uncaught SyntaxError: Unexpected ' + tokens[current].value);
           }
 
         } else {
           throw new TypeError('Uncaught SyntaxError: Unexpected ' + tokens[current].type);
         }
 
+      } else {
+        throw new TypeError('Uncaught SyntaxError: Unexpected ' + tokens[current].type);
+      }
 
+    }
+
+  } else {
+    throw new TypeError('Uncaught SyntaxError: Unexpected ' + token.type);
+  }
+
+  ++current;
+  if (tokens[current].type === 'Punctuator' && tokens[current].value === '{') {
+    Object.assign(node.body, {
+      "type": "BlockStatement",
+      "body": []
+    });
+    const blockBody = node.body.body;
+
+    ++current;
+    if (tokens[current].type === 'Keyword' && tokens[current].value === 'return') {
+      blockBody.push({
+        "type": "ReturnStatement",
+        "argument": {}
+      });
+
+      const returnArguments = blockBody[0].argument;
+
+      if (tokens[current].type === 'Identifier' && tokens[current].value === 'console') {
+        ++current;
+        if (tokens[current].type === 'Punctuator' && tokens[current].value === '.') {
+          ++current;
+          if (tokens[current].type === 'Identifier' && tokens[current].value === 'log') {
+            ++current;
+            if (tokens[current].type === 'Punctuator' && tokens[current].value === '(') {
+              Object.assign(returnArguments, {
+                "type": "CallExpression",
+                "callee": {
+                  "type": "MemberExpression",
+                  "computed": false,
+                  "object": {
+                    "type": "Identifier",
+                    "name": "console"
+                  },
+                  "property": {
+                    "type": "Identifier",
+                    "name": "log"
+                  }
+                },
+                "arguments": []
+              });
+              const callArguments = returnArguments.arguments;
+
+              ++current;
+
+              while (
+                tokens[current].type === 'String' || 
+                tokens[current].type === 'Identifier'
+              ) {
+                ++current;
+
+                if (tokens[current].type === 'Punctuator') {
+
+                  switch(tokens[current].value) {
+                    case ',':
+                      callArguments.push({ "type": "Identifier", "name": tokens[current-1].value });
+                      ++current;
+                      continue;
+                    case '+':
+                      ++current;
+                      while (
+                        tokens[current].type === 'Identifier' || 
+                        tokens[current].type === 'String'
+                      ) {
+                        let left, right;
+                        if (tokens[current-2].type === 'Identifier') {
+                          left = {
+                            "type": "Identifier",
+                            "name": tokens[current-2].value
+                          }
+                        } else {
+                          left = {
+                            "type": "Literal",
+                            "value": tokens[current-2].value,
+                            "raw": tokens[current-2].value
+                          }
+                        }
+
+                        if (tokens[current].type === 'Identifier') {
+                          right = {
+                            "type": "Identifier",
+                            "name": tokens[current].value
+                          }
+                        } else {
+                          right = {
+                            "type": "Literal",
+                            "value": tokens[current].value,
+                            "raw": tokens[current].value
+                          }
+                        }
+                        callArguments.push({ 
+                          "type": "BinaryExpression", 
+                          "operator": "+",
+                          "left": left,
+                          "right": right
+                        });
+
+                      }
+                      break;
+                    case ')':
+                      callArguments.push({ "type": "Literal", "value": tokens[current-1].value, "raw": tokens[current-1].value });
+                      break;
+                    default:
+                      throw new TypeError('Uncaught SyntaxError: Unexpected ' + token.value);
+                  }
+
+                } else {
+                  throw new TypeError('Uncaught SyntaxError: Unexpected ' + token.type);
+                }
+
+              }
+
+            } else {
+              throw new TypeError('Uncaught SyntaxError: Unexpected ' + token.value);
+            }
+          } else {
+            throw new TypeError('Uncaught SyntaxError: Unexpected ' + token.value);
+          }
+        } else {
+          throw new TypeError('Uncaught SyntaxError: Unexpected ' + token.value);
+        }
+      } else {
+        throw new TypeError('Uncaught SyntaxError: Unexpected ' + token.value);
       }
 
     } else {
-      throw new TypeError('Uncaught SyntaxError: Unexpected ' + token.type);
+      throw new TypeError('Uncaught SyntaxError: Unexpected ' + token.value);
     }
+
+  } else {
+    throw new TypeError('Uncaught SyntaxError: Unexpected ' + token.type);
   }
-
-  return walk();
-
 
 }
 
-console.dir(parser(tokens1));
+console.dir(parser(tokens));
