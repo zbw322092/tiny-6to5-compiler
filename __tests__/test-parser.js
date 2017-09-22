0fc62f08fc40121c14bc2dc8a0acc6645b6ce845
Bowen Zhong <bz4e14@soton.ac.uk>
2017-09-23 00:51:00 +0800

    first ugly but working parser version

---
+++ __tests__/test-parser.js
@@ -0,0 +1,98 @@
+const fs = require('fs');
+const path = require('path');
+const parser = require('../parser.js');
+
+const expected1 = {
+  "type": "FunctionDeclaration",
+  "id": {
+    "type": "Identifier",
+    "name": "hi"
+  },
+  "params": [
+    {
+      "type": "AssignmentPattern",
+      "left": {
+        "type": "Identifier",
+        "name": "name"
+      },
+      "right": {
+        "type": "Literal",
+        "value": "Bo",
+        "raw": "'Bo'"
+      }
+    },
+    {
+      "type": "AssignmentPattern",
+      "left": {
+        "type": "Identifier",
+        "name": "age"
+      },
+      "right": {
+        "type": "Literal",
+        "value": "18",
+        "raw": "'18'"
+      }
+    }
+  ],
+  "body": {
+    "type": "BlockStatement",
+    "body": [
+      {
+        "type": "ReturnStatement",
+        "argument": {
+          "type": "CallExpression",
+          "callee": {
+            "type": "MemberExpression",
+            "object": {
+              "type": "Identifier",
+              "name": "console"
+            },
+            "property": {
+              "type": "Identifier",
+              "name": "log"
+            }
+          },
+          "arguments": [
+            {
+              "type": "BinaryExpression",
+              "operator": "+",
+              "left": {
+                "type": "BinaryExpression",
+                "operator": "+",
+                "left": {
+                  "type": "BinaryExpression",
+                  "operator": "+",
+                  "left": {
+                    "type": "Literal",
+                    "value": "hi there, it is ",
+                    "raw": "'hi there, it is '"
+                  },
+                  "right": {
+                    "type": "Identifier",
+                    "name": "name"
+                  }
+                },
+                "right": {
+                  "type": "Literal",
+                  "value": " I am ",
+                  "raw": "' I am '"
+                }
+              },
+              "right": {
+                "type": "Identifier",
+                "name": "age"
+              }
+            }
+          ]
+        }
+      }
+    ]
+  }
+};
+
+const result1 = parser;
+
+
+test('parser', () => {
+  expect(result1).toEqual(expected1);
+});
--- parser.js
+++ parser.js
@@ -109,52 +109,6 @@ const tokens = [
   }
 ];
 
-const tokens1 = [
-  {
-    "type": "Keyword",
-    "value": "function"
-  },
-  {
-    "type": "Identifier",
-    "value": "hi"
-  },
-  {
-    "type": "Punctuator",
-    "value": "("
-  },
-  {
-    "type": "Identifier",
-    "value": "name"
-  },
-  {
-    "type": "Punctuator",
-    "value": "="
-  },
-  {
-    "type": "String",
-    "value": "'Bo'"
-  },
-  {
-    "type": "Punctuator",
-    "value": ","
-  },
-  {
-    "type": "Identifier",
-    "value": "age"
-  },
-  {
-    "type": "Punctuator",
-    "value": "="
-  },
-  {
-    "type": "String",
-    "value": "'18'"
-  },
-  {
-    "type": "Punctuator",
-    "value": ")"
-  }
-];
 
 function parser(tokens) {
   let current = 0;
@@ -205,7 +159,7 @@ function parser(tokens) {
                       },
                       "right": {
                         "type": "Literal",
-                        "value": tokens[current + 2].value,
+                        "value": tokens[current + 2].value.slice(1,-1),
                         "raw": tokens[current + 2].value
                       }
                     });
@@ -243,6 +197,8 @@ function parser(tokens) {
             throw new TypeError('Uncaught SyntaxError: Unexpected ' + tokens[current].value);
           }
 
+          console.log(node);
+
         } else {
           throw new TypeError('Uncaught SyntaxError: Unexpected ' + tokens[current].type);
         }
@@ -273,7 +229,7 @@ function parser(tokens) {
       });
 
       const returnArguments = blockBody[0].argument;
-
+      ++current;
       if (tokens[current].type === 'Identifier' && tokens[current].value === 'console') {
         ++current;
         if (tokens[current].type === 'Punctuator' && tokens[current].value === '.') {
@@ -285,7 +241,6 @@ function parser(tokens) {
                 "type": "CallExpression",
                 "callee": {
                   "type": "MemberExpression",
-                  "computed": false,
                   "object": {
                     "type": "Identifier",
                     "name": "console"
@@ -302,37 +257,29 @@ function parser(tokens) {
               ++current;
 
               while (
-                tokens[current].type === 'String' || 
+                tokens[current].type === 'String' ||
                 tokens[current].type === 'Identifier'
               ) {
                 ++current;
 
                 if (tokens[current].type === 'Punctuator') {
 
-                  switch(tokens[current].value) {
+                  switch (tokens[current].value) {
                     case ',':
-                      callArguments.push({ "type": "Identifier", "name": tokens[current-1].value });
+                      callArguments.push({ "type": "Identifier", "name": tokens[current - 1].value });
                       ++current;
                       continue;
                     case '+':
-                      ++current;
-                      while (
-                        tokens[current].type === 'Identifier' || 
+                      if (!(callArguments.length >= 1 && callArguments[callArguments.length - 1].type === 'BinaryExpression')) {
+                        ++current;
+                      } else {
+                        --current;
+                      }
+                      if (
+                        tokens[current].type === 'Identifier' ||
                         tokens[current].type === 'String'
                       ) {
                         let left, right;
-                        if (tokens[current-2].type === 'Identifier') {
-                          left = {
-                            "type": "Identifier",
-                            "name": tokens[current-2].value
-                          }
-                        } else {
-                          left = {
-                            "type": "Literal",
-                            "value": tokens[current-2].value,
-                            "raw": tokens[current-2].value
-                          }
-                        }
 
                         if (tokens[current].type === 'Identifier') {
                           right = {
@@ -342,21 +289,85 @@ function parser(tokens) {
                         } else {
                           right = {
                             "type": "Literal",
-                            "value": tokens[current].value,
+                            "value": tokens[current].value.slice(1,-1),
                             "raw": tokens[current].value
                           }
                         }
-                        callArguments.push({ 
-                          "type": "BinaryExpression", 
-                          "operator": "+",
-                          "left": left,
-                          "right": right
-                        });
-
+                        if (callArguments.length >= 1 && callArguments[callArguments.length - 1].type === 'BinaryExpression') {
+                          callArguments[callArguments.length - 1] = {
+                            "type": "BinaryExpression",
+                            "operator": "+",
+                            left: callArguments[callArguments.length - 1],
+                            right: right
+                          }
+                        } else {
+                          if (tokens[current - 2].type === 'Identifier') {
+                            left = {
+                              "type": "Identifier",
+                              "name": tokens[current - 2].value
+                            }
+                          } else {
+                            left = {
+                              "type": "Literal",
+                              "value": tokens[current - 2].value.slice(1,-1),
+                              "raw": tokens[current - 2].value
+                            }
+                          }
+                          callArguments.push({
+                            "type": "BinaryExpression",
+                            "operator": "+",
+                            "left": left,
+                            "right": right
+                          });
+                        }
+                      }
+                      if (tokens[current + 1].type === 'Punctuator') {
+                        if (tokens[current + 1].value === ',') {
+                          current += 2;
+                          continue;
+                        } else if (tokens[current + 1].value === ')') {
+                          break;
+                        } else if (tokens[current + 1].value === '+') {
+                          current += 2;
+                          continue;
+                        } else {
+                          throw new TypeError('Uncaught SyntaxError: Unexpected ' + tokens[current + 1].value);
+                        }
+                      } else {
+                        throw new TypeError('Uncaught SyntaxError: Unexpected ' + token.type);
                       }
                       break;
                     case ')':
-                      callArguments.push({ "type": "Literal", "value": tokens[current-1].value, "raw": tokens[current-1].value });
+                      if (callArguments.length >= 1 && callArguments[callArguments.length - 1].type === 'BinaryExpression') {
+
+                        let right;
+
+                        if (tokens[current-1].type === 'Identifier') {
+                          right = {
+                            "type": "Identifier",
+                            "name": tokens[current-1].value
+                          }
+                        } else {
+                          right = {
+                            "type": "Literal",
+                            "value": tokens[current-1].value.slice(1,-1),
+                            "raw": tokens[current-1].value
+                          }
+                        }
+
+                        callArguments[callArguments.length - 1] = {
+                          "type": "BinaryExpression",
+                          "operator": "+",
+                          left: callArguments[callArguments.length - 1],
+                          right: right
+                        }
+                        break;
+                      }
+                      callArguments.push({ 
+                        "type": "Literal", 
+                        "value": tokens[current - 1].value.slice(1,-1), 
+                        "raw": tokens[current - 1].value 
+                      });
                       break;
                     default:
                       throw new TypeError('Uncaught SyntaxError: Unexpected ' + token.value);
@@ -389,6 +400,7 @@ function parser(tokens) {
     throw new TypeError('Uncaught SyntaxError: Unexpected ' + token.type);
   }
 
+  return node;
 }
 
-console.dir(parser(tokens));
+module.exports = parser(tokens);
