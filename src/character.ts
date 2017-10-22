250be17b8712441e79d41db6eaafb86fd4bb4da1
Bowen Zhong <bz4e14@soton.ac.uk>
2017-10-22 22:07:05 +0800

    scan comment

--- src/character.ts
+++ src/character.ts
@@ -12,6 +12,8 @@ const Regex = {
  * reference url: 
  * 1. https://mathiasbynens.be/notes/javascript-encoding#surrogate-formulae
  * 2. https://www.ecma-international.org/ecma-262/8.0/index.html#sec-utf16encoding
+ * 
+ * @param cp 
  */
 export const fromCodePoint = (cp: number): string => {
   if (cp >= 0x0000 && cp <= 0xFFFF) {
@@ -23,3 +25,52 @@ export const fromCodePoint = (cp: number): string => {
   }
 };
 
+/**
+ * reference url:
+ * https://www.ecma-international.org/ecma-262/8.0/index.html#sec-white-space
+ * 
+ * @param cp 
+ */
+export const isWhiteSpace = (cp: number): boolean => {
+  return (cp === 0x20) || (cp === 0x09) || (cp === 0x0B) || (cp === 0x0C) || (cp === 0xA0) ||
+    (cp >= 0x1680 && [0x1680, 0x2000, 0x2001, 0x2002, 0x2003, 0x2004, 0x2005, 0x2006, 0x2007, 0x2008, 0x2009, 0x200A, 0x202F, 0x205F, 0x3000, 0xFEFF].indexOf(cp) >= 0);
+};
+
+/**
+ * reference url:
+ * https://www.ecma-international.org/ecma-262/8.0/index.html#sec-identifier-names
+ * 
+ * @param cp
+ */
+export const isIdentifierStart = (cp: number): boolean => {
+  return (cp === 0x24) || (cp === 0x5F) ||  // $ (dollar) and _ (underscore)
+    (cp >= 0x41 && cp <= 0x5A) ||         // A..Z
+    (cp >= 0x61 && cp <= 0x7A) ||         // a..z
+    (cp === 0x5C) ||                      // \ (backslash)
+    ((cp >= 0x80) && Regex.NonAsciiIdentifierStart.test(fromCodePoint(cp)));
+};
+
+/**
+ * reference url:
+ * https://www.ecma-international.org/ecma-262/8.0/index.html#sec-identifier-names
+ * 
+ * @param cp
+ */
+export const isIdentifierPart = (cp: number): boolean => {
+  return (cp === 0x24) || (cp === 0x5F) ||  // $ (dollar) and _ (underscore)
+    (cp >= 0x41 && cp <= 0x5A) ||         // A..Z
+    (cp >= 0x61 && cp <= 0x7A) ||         // a..z
+    (cp >= 0x30 && cp <= 0x39) ||         // 0..9
+    (cp === 0x5C) ||                      // \ (backslash)
+    ((cp >= 0x80) && Regex.NonAsciiIdentifierPart.test(fromCodePoint(cp)));
+};
+
+/**
+ * reference url:
+ * https://www.ecma-international.org/ecma-262/8.0/index.html#sec-line-terminators
+ * 
+ * @param cp 
+ */
+export const isLineTerminator = (cp: number): boolean => {
+  return (cp === 0x000A) || (cp === 0x000D) || (cp === 0x2028) || (cp === 0x2029);
+};
--- src/scanner.ts
+++ src/scanner.ts
@@ -2,6 +2,29 @@ const fs = require('fs');
 const path = require('path');
 const src = fs.readFileSync(path.join(__dirname, '../__tests__/files/file.js'), 'utf8');
 
+import { 
+  isLineTerminator,
+  isWhiteSpace
+} from './character';
+
+interface Position {
+  line: number;
+  column: number
+}
+
+interface SourceLocation {
+  start: Position;
+  end: Position;
+  source?: string;
+}
+
+interface Comment {
+  multiLine: boolean;
+  slice: number[];
+  range: [number, number];
+  loc: SourceLocation;
+}
+
 interface ScannerState {
   index: number;
   lineNumber: number;
@@ -48,6 +71,66 @@ class Scanner {
   }
 
   public scanComments() {
+    let comments;
+    if (this.trackComment) {
+      comments = [];
+    }
 
+    let start: boolean = (this.index === 0);
+    while (!this.eof()) {
+      let ch = this.source.charCodeAt(this.index);
+
+      if (isWhiteSpace(ch)) {
+        ++this.index;
+      } else if (isLineTerminator(ch)) {
+        /** reference url:
+         * https://www.ecma-international.org/ecma-262/8.0/index.html#sec-line-terminators
+         * 
+         * The sequence <CR><LF> is commonly used as a line terminator. 
+         * It should be considered a single SourceCharacter for the purpose of reporting line numbers.
+         */
+        ++this.index;
+        if (ch === 0x000D && this.source.charCodeAt(this.index) === 0x000A) {
+          ++this.index;
+        }
+        ++this.lineNumber;
+        this.lineStart = this.index;
+        start = true;
+      } else if (ch === 0x0002F /* 0x0002F is / */) {
+        ch = this.source.charCodeAt(this.index + 1);
+        if (ch === 0x0002F) {
+          // do something
+        } else if (ch === 0x002A /* 0x002A is * */) {
+          // do something
+        } else {
+          break; // is not comment
+        }
+      } else if (start && ch === 0x002D /* 0x002D is - */) {
+        // 0x003E is >
+        if (
+          (this.source.charCodeAt(this.index + 1) === 0x002D) && 
+          (this.source.charCodeAt(this.index + 2) === 0x003E)
+        ) {
+          // '-->' is a single-line comment
+          // do something
+        } else {
+          break; // is not comment
+        }
+      } else if (ch === 0x003C /* 0x003C is < */) {
+        if (this.source.slice(this.index+1, this.index + 4) === '!--') {
+          // is comment like <!--
+          this.index += 4;
+          // do something
+        } else {
+          break; // is not comment
+        }
+      } else {
+        break; // not handle and is not comment
+      }
+    }
+
+    return comments;
   }
+
+  
 }
